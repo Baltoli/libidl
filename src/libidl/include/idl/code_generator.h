c1b097e5c52109cd2ad8af8a74b0df1173485ada
Bruce Collie <brucecollie82@gmail.com>
2019-05-15 11:58:14 +0100

    Start to do code generation

--- src/libidl/include/idl/code_generator.h
+++ src/libidl/include/idl/code_generator.h
@@ -1,8 +1,31 @@
 #pragma once
 
 #include <idl/internal/code_gen_src.h>
+#include <idl/specification.h>
 
 #include <Python.h>
 
 namespace idl {
-}
+
+class code_generator {
+public:
+  /**
+   * Construct a new code generator that can turn an IDL specification into c++
+   * code to be JIT compiled.
+   *
+   * This constructor is responsible for launching a python interpreter and
+   * setting up the state required to use it, so it might be expensive to call.
+   */
+  code_generator();
+
+  /**
+   * Shut down the python interpreter when we're done generating code.
+   */
+  ~code_generator();
+
+  std::string generate_from(specification const&) const;
+
+private:
+};
+
+} // namespace idl
--- src/libidl/src/code_generator.cpp
+++ src/libidl/src/code_generator.cpp
@@ -1,4 +1,14 @@
 #include <idl/code_generator.h>
 
 namespace idl {
+
+code_generator::code_generator() {}
+
+code_generator::~code_generator() {}
+
+std::string code_generator::generate_from(specification const& s) const
+{
+  return s.to_string();
 }
+
+} // namespace idl
